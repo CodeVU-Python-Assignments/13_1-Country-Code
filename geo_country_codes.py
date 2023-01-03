1368cef6f16806c947beb1303b75e7df6dfa3c87
Nathan Gearke <69879313+GearsandKeys@users.noreply.github.com>
2023-01-03 13:32:12 -0600

    Update geo_country_codes.py

--- geo_country_codes.py
+++ geo_country_codes.py
@@ -1,9 +1,14 @@
-# Don't forget your imports!
+import urllib.request
+import urllib.parse
+import urllib.error
+import json
 
 def print_country_code_from_geocoding() -> None:
     # I have included the XML and JSON example files, 
     # Feel free to start with one of those, and make the changes needed.
+    print('implement me')
 
 
 # The original examples are a loop, write this to run once
-print_country_code_from_geocoding()
+if __name__ == "main":
+    print_country_code_from_geocoding()
