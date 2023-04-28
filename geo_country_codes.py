86747ccc820af47ae7be2a4bd392e0361e6d31f1
Nathan Gearke <69879313+GearsandKeys@users.noreply.github.com>
2023-04-28 12:49:50 -0500

    Update geo_country_codes.py

--- geo_country_codes.py
+++ geo_country_codes.py
@@ -4,16 +4,7 @@ import urllib.error
 import json
 
 def print_country_code_from_geocoding() -> None:
-    api_key = False
-    # If you have a Google Places API key, enter it here
-    # api_key = 'AIzaSy___IDByT70'
-    # https://developers.google.com/maps/documentation/geocoding/intro
-
-    if api_key is False:
-        api_key = 42
-        serviceurl = 'http://py4e-data.dr-chuck.net/json?'
-    else :
-        serviceurl = 'https://maps.googleapis.com/maps/api/geocode/json?'
+    # Feel free to use json or xml and check the example files
         
         
     print('implement me')
